33c4236ca846add744f21177b5e0872ee3961162
Jose Melgares <rjmzelaya@gmail.com>
2026-04-05 17:31:04 +0200

    feat: agrega clase cine

--- entregas/melgaresJose/src/Cine.java
+++ entregas/melgaresJose/src/Cine.java
@@ -1,2 +1,19 @@
 class Cine {
+    Pelicula[] repertorio;
+    Sala[] salas;
+    Proyeccion[] exhibiciones;
+    String nombre; 
+    
+    public Cine(Pelicula[] peliculas, Sala[] salas, Proyeccion[] proyecciones, String nombre) {
+        repertorio = peliculas;
+        this.salas = salas;
+        exhibiciones = proyecciones;
+        this.nombre = nombre;
+    }
+    public Cine(Sala[] salas, String nombre) {
+        this(null, salas, null, nombre);
+    }
+    public Cine(String nombre) {
+        this(null, nombre);
+    }
 }
