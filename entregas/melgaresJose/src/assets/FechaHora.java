fc0713db52dcd9e183fb5bd959947ac4678fd90a
Jose Melgares <rjmzelaya@gmail.com>
2026-04-05 16:07:36 +0200

    feat: agrega clase FechaHora

---
+++ entregas/melgaresJose/src/assets/FechaHora.java
@@ -0,0 +1,30 @@
+package assets;
+
+class FechaHora {
+    int dia;
+    int mes;
+    int año;
+    int hora;
+    int minuto;
+
+    public FechaHora(int dia, int mes, int año, int hora, int minuto) {
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
+        this.hora = hora;
+        this.minuto = minuto;
+    }
+
+    public FechaHora(int dia, int mes, int año) {
+        this(dia, mes, año, 0, 0);
+    }
+
+    public FechaHora(FechaHora fechaHora) {
+        this(fechaHora.dia,
+            fechaHora.mes,
+            fechaHora.año,
+            fechaHora.hora,
+            fechaHora.minuto
+        );
+    }
+}
