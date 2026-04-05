dcc31d869e63b9b0fb8575da9c18a1e9b7ec4939
Jose Melgares <rjmzelaya@gmail.com>
2026-04-05 15:14:24 +0200

    feat: implementacion clase Pelicula

---
+++ entregas/melgaresJose/src/Pelicula.java
@@ -0,0 +1,45 @@
+ import assets.Console;
+
+class Pelicula {   
+    String titulo;
+    String director;
+    int duracion;
+    int clasificacion;
+
+    Console consola = new Console();
+
+    public Pelicula(String titulo, String director, int duracion, int clasificacion) {
+        this.titulo = titulo;
+        this.director = director;
+        this.duracion = duracion;
+        this.clasificacion = clasificacion;
+    }
+    public Pelicula(String titulo, String director) {
+        this(titulo, director, 0, 0);
+    }
+    public Pelicula(String titulo) {
+        this(titulo, "");
+    }
+
+    public void mostrarInformacion() {
+        consola.write(formatearAString());
+    }
+    public boolean esMasLarga(Pelicula pelicula) {
+        return duracion > pelicula.duracion;
+    }
+    public void cambiarClasificacion(int clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    private String formatearAString() {
+        String seccionTitulo = "Titulo: " + titulo;
+        String seccionDirector = "Dirigida por: " + director;
+        String seccionDuracion = "Duracion: " + duracion + "min";
+        String seccionClasificacion = "Clasificación: " + clasificacion;
+
+        return seccionTitulo + " // \n" 
+                + seccionDirector + " // \n"
+                + seccionDuracion + " // \n" 
+                + seccionClasificacion;
+    }
+}
